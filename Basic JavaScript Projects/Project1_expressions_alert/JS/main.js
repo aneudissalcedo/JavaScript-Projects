143a8a5b57ae4aa5df41aa90e4f0d6fe86364c75
aneudissalcedo <aneudissalcedo@gmail.com>
2022-02-02 18:56:51 -0500

    Added some changes to the main.js file, now it gives the paragraph a color, from white to green.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -10,12 +10,10 @@ var textBox = s + s2;
 var x = 3;
 var expression = x + 5;
 
-/**Unavailable to change fonts color since it has been removed
- * due to some browsers not supporting its compability. 
+
 // var textColor = "This is a paragraph!";
-// var result = textColor.fontcolor("green"); 
-// document.getElementById("fontColor").style.color = 'green';
-*/
+// var result = textColor.fontcolor("green");
+document.getElementById("fontColor").style.color = 'green';
 
 /* Unavailable to change fonts color
 let a = "Hello";
@@ -25,6 +23,7 @@ document.getElementById("fontColor").innerHTML = result;
 
 // Shows a Box Alert
 window.alert(textBox);
+document.write(textBox);
 // document.write(A);
 // Writes documentation/paragraph in the website body with the content inside.
 document.write(expression);
