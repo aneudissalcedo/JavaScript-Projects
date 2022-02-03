aab153f7c0dd8e08ba9a5b9ffea81541d52b71fb
aneudissalcedo <aneudissalcedo@gmail.com>
2022-02-03 15:16:59 -0500

    Added the Project2_functions and its content to the repository.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,25 @@
+
+
+//Add the variables x and y, and return its sum.
+function AddTwoVariables(){
+    //Declaration of variables
+    var x = 5;
+    var y = 3;
+    var result = x + y;
+
+    //Shows the behind operation to the user in form of document
+    document.getElementById("ButtonVariables").innerHTML = result;
+}
+
+//Concatination of strings
+function Concatenated(){
+    var s = "I'm using ";
+        s += "JavaScript";
+
+    document.getElementById("Concatenated").innerHTML = s;
+}
+
+
+
+
+
